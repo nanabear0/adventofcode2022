94200eaf2a70a97ec10e240a0753466829c92e75
Denizhan Erdem <derdem@microsoft.com>
2022-11-30 11:31:25 +0000

    initial commit

--- adventofcode2022/Program.cs
+++ adventofcode2022/Program.cs
@@ -1,4 +1,6 @@
-﻿internal class Program
+﻿using adventofcode2022.day01;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
---
+++ adventofcode2022/day01/Day01.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventofcode2022.day01;
+
+internal class Day01
+{
+}
