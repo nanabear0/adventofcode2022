7a1729d64eeb6836d486db51f7598a769b730aea
Denizhan Erdem <derdem@microsoft.com>
2022-12-01 10:38:41 +0000

    day01 complete

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,7 +1,32 @@
-﻿internal class Program
+﻿using System.Diagnostics.Metrics;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Day 01 Placeholder");
+        var elves = new List<List<int>>();
+        elves.Add(new List<int>());
+        foreach (string line in System.IO.File.ReadLines(@"input.txt"))
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                elves.Add(new List<int>());
+            }
+            else
+            {
+                elves.Last().Add(int.Parse(line));
+            }
+        }
+        var maxCal = elves
+            .Select(x => x.Sum())
+            .Max();
+        Console.WriteLine("p1:" + maxCal);
+
+        var maxCal3 = elves
+            .Select(x => x.Sum())
+            .OrderByDescending(x => x)
+            .Take(3)
+            .Sum();
+        Console.WriteLine("p2:" + maxCal3);
     }
 }
