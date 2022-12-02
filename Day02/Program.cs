c10302a94803c72b23e62e03c47f6640a65d6e6d
Denizhan Erdem <derdem@microsoft.com>
2022-12-02 09:56:31 +0000

    day02

---
+++ Day02/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        day01();
+        day02();
+    }
+
+    public static void day02()
+    {
+        Console.WriteLine(
+            "day01: " +
+            System.IO.File.ReadLines(@"input.txt")
+            .Select(x => x.Split(' '))
+            .Select(line => (ec: (int)line[0][0] - 65, result: (int)line[1][0] - 88))
+            .Select(x => x.result * 3 + (x.ec + x.result + 2) % 3 + 1)
+            .Sum());
+    }
+    public static void day01()
+    {
+        Console.WriteLine(
+            "day02: " +
+            System.IO.File.ReadLines(@"input.txt")
+            .Select(x => x.Split(' '))
+            .Select(line => (ec: (int)line[0][0] - 65, mc: (int)line[1][0] - 88))
+            .Select(x => x.mc + 1 + ((x.mc - x.ec + 4) % 3) * 3)
+            .Sum());
+    }
+}
