a4bf6da14e175ea29fa03d426c139752d962c2c4
Denizhan Erdem <denizhanerdem@gmail.com>
2022-12-03 15:57:13 +0000

    refzctor

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Runtime.CompilerServices;
-
-internal class Program
+﻿internal class Program
 {
     private static void Main(string[] args)
     {
@@ -12,32 +10,21 @@ internal class Program
     {
         var sum = File.ReadLines("input.txt")
             .Chunk(3)
-            .Select(e => e.Select(s => s.ToHashSet()).ToList())
-            .Select(elves => elves[0].Intersect(elves[1]).Intersect(elves[2])
-                .Select(charValue).First())
+            .Select(elves => elves[0].Intersect(elves[1]).Intersect(elves[2]).First())
+            .Select(charValue)
             .Sum();
         Console.WriteLine($"part2:{sum}");
     }
+
     private static void part01()
     {
         var sum = File.ReadLines("input.txt")
-            .Select(line => line.Substring(0, line.Length / 2).ToHashSet()
-                .Intersect(line.Substring(line.Length / 2).ToHashSet())
-                .Select(charValue).First())
+            .Select(line => line.Chunk(line.Length / 2))
+            .Select(chunks => chunks.First().Intersect(chunks.Last()).First())
+            .Select(charValue)
             .Sum();
         Console.WriteLine($"part1:{sum}");
     }
 
-    private static int charValue(char c)
-    {
-        if (c >= 'a' && c <= 'z')
-        {
-            return (int)c - (int)'a' + 1;
-        }
-        else if (c >= 'A' && c <= 'Z')
-        {
-            return (int)c - (int)'A' + 27;
-        }
-        return 0;
-    }
+    private static int charValue(char c) => Char.IsLower(c) ? c - 'a' + 1 : c - 'A' + 27;
 }
