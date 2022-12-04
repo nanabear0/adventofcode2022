4f3256f3f50475119ca155d34c7227b7f29167ef
Denizhan Erdem <denizhanerdem@gmail.com>
2022-12-04 12:44:25 +0000

    refactor

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -23,22 +23,16 @@ internal class Program
         Console.WriteLine($"part2: {count}");
     }
 
-    private static List<int> getAssignments(string line)
-    {
-        return new Regex(@"[0-9]+")
+    private static List<int> getAssignments(string line) =>
+        new Regex(@"[0-9]+")
             .Matches(line)
             .Select(x => int.Parse(x.Value))
             .ToList();
-    }
 
-    private static Boolean doesContain(int r1s, int r1e, int r2s, int r2e)
-    {
-        return (r1s <= r2s && r1e >= r2e) || (r1s >= r2s && r1e <= r2e);
-    }
+    private static Boolean doesContain(int r1s, int r1e, int r2s, int r2e) =>
+        (r1s <= r2s && r1e >= r2e) || (r1s >= r2s && r1e <= r2e);
 
-    private static Boolean doesOverlap(int r1s, int r1e, int r2s, int r2e)
-    {
-        return (r1s >= r2s && r1s <= r2e) || (r2s >= r1s && r2s <= r1e);
-    }
+    private static Boolean doesOverlap(int r1s, int r1e, int r2s, int r2e) =>
+        (r1s >= r2s && r1s <= r2e) || (r2s >= r1s && r2s <= r1e);
 
 }
