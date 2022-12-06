d012b507cb29f3d7f8e03dcbc487fd0664c19e37
Denizhan Erdem <derdem@microsoft.com>
2022-12-06 10:28:34 +0000

    indent

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -9,7 +9,7 @@
     private static void part(int p, int c)
     {
         var stream = File.ReadAllText("input.txt").ToList();
-        var count = Enumerable.Range(0, stream.Count - (c-1))
+        var count = Enumerable.Range(0, stream.Count - (c - 1))
             .TakeWhile(i => stream.GetRange(i, c).ToHashSet().Count != c)
             .Count();
         Console.WriteLine($"part{p}: {count + c}");
