e591dffcab00a011992db5975cf41ddb84ad2f51
Denizhan Erdem <derdem@microsoft.com>
2022-12-06 10:18:44 +0000

    day 6: poop day

---
+++ Day06/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        part(1, 4);
+        part(2, 14);
+    }
+
+    private static void part(int p, int c)
+    {
+        var stream = File.ReadAllText("input.txt").ToList();
+        var count = Enumerable.Range(0, stream.Count - (c-1)).TakeWhile(i => stream.GetRange(i, c).ToHashSet().Count != c).Count();
+        Console.WriteLine($"part{p}: {count + c}");
+    }
+}
