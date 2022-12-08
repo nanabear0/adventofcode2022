a158a1a5e2348fa148cfd534cf747ea7d4092b8b
Denizhan Erdem <denizhanerdem@gmail.com>
2022-12-08 21:56:11 +0000

    very stupid day8

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -7,67 +7,29 @@
     }
     private static void part2()
     {
-        List<List<int>> trees = new List<List<int>>();
-        foreach (string line in File.ReadLines("input.txt"))
-            trees.Add(line.Select(x => x - '0').ToList());
-
-        int maxScore = -1;
-
-        for (int i = 0; i < trees.Count; i++)
+        List<List<int>> trees = File.ReadLines("input.txt").Select(line => (line.Select(x => x - '0').ToList())).ToList();
+        int maxScore = trees.SelectMany((treeRow, i) => treeRow.Select((tree, j) =>
         {
-            for (int j = 0; j < trees[i].Count; j++)
-            {
-                var upCount = 0;
-                for (int u = i - 1; u >= 0; u--)
-                {
-                    upCount++;
-                    if (trees[i][j] <= trees[u][j]) break;
-                }
-                var downCount = 0;
-                for (int d = i + 1; d < trees.Count; d++)
-                {
-                    downCount++;
-                    if (trees[i][j] <= trees[d][j]) break;
-                }
-                var leftCount = 0;
-                for (int l = j - 1; l >= 0; l--)
-                {
-                    leftCount++;
-                    if (trees[i][j] <= trees[i][l]) break;
-                }
-                var rightCount = 0;
-                for (int r = j + 1; r < trees[i].Count; r++)
-                {
-                    rightCount++;
-                    if (trees[i][j] <= trees[i][r]) break;
-                }
-                var score = upCount * downCount * leftCount * rightCount;
-                maxScore = maxScore < score ? score : maxScore;
-            }
-        }
+            var upCount = trees.Select(v => v[j]).Take(i).Reverse().TakeWhile(v => v < trees[i][j]).Count();
+            if (upCount != i) upCount++;
+            var downCount = trees.Select(v => v[j]).Skip(i + 1).TakeWhile(v => v < trees[i][j]).Count();
+            if (downCount != (trees.Count - i - 1)) downCount++;
+            var leftCount = trees[i].Take(j).Reverse().TakeWhile(v => v < trees[i][j]).Count();
+            if (leftCount != j) leftCount++;
+            var rightCount = trees[i].Skip(j + 1).TakeWhile(v => v < trees[i][j]).Count();
+            if (rightCount != (trees[i].Count - j - 1)) rightCount++;
+            return upCount * downCount * leftCount * rightCount;
+        })).Max();
         Console.WriteLine($"part2: {maxScore}");
     }
     private static void part1()
     {
-        List<List<int>> trees = new List<List<int>>();
-        foreach (string line in File.ReadLines("input.txt"))
-            trees.Add(line.Select(x => x - '0').ToList());
-
-        int count = 0;
-        for (int i = 0; i < trees.Count; i++)
-        {
-            for (int j = 0; j < trees.Count; j++)
-            {
-                // Up
-                if (trees.Select(v => v[j]).Take(i).All(v => v < trees[i][j])) { count++; continue; }
-                // Down
-                if (trees.Select(v => v[j]).Skip(i + 1).All(v => v < trees[i][j])) { count++; continue; }
-                // Left
-                if (trees[i].Take(j).All(v => v < trees[i][j])) { count++; continue; }
-                // Right
-                if (trees[i].Skip(j + 1).All(v => v < trees[i][j])) { count++; continue; }
-            }
-        }
+        List<List<int>> trees = File.ReadLines("input.txt").Select(line => (line.Select(x => x - '0').ToList())).ToList();
+        int count = trees.Select((treeRow, i) => treeRow.Where((tree, j) =>
+                        trees.Select(v => v[j]).Take(i).All(v => v < tree) ||
+                        trees.Select(v => v[j]).Skip(i + 1).All(v => v < tree) ||
+                        trees[i].Take(j).All(v => v < tree) ||
+                        trees[i].Skip(j + 1).All(v => v < tree)).Count()).Sum();
         Console.WriteLine($"part1: {count}");
     }
 }
