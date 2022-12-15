cdb39e29c715e795584c3aaf9429b86f9d1af85b
Denizhan Erdem <derdem@microsoft.com>
2022-12-15 15:01:14 +0000

    day 15 , part 2

--- Day15/Program.cs
+++ Day15/Program.cs
@@ -4,7 +4,44 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        part1();
+        //part1();
+        part2();
+    }
+
+    private static void part2()
+    {
+        HashSet<((int, int), int)> sensors = new();
+        long range = 4000000;
+        foreach (string line in File.ReadLines("input.txt"))
+        {
+            int[] numbers = new Regex(@"-?[0-9]+").Matches(line).Select(m => int.Parse(m.Value)).ToArray();
+            var sensor = (numbers[0], numbers[1]);
+            var beacon = (numbers[2], numbers[3]);
+            var distance = manhattanDistance(sensor, beacon);
+            sensors.Add((sensor, distance));
+        }
+        for (int y = 0; y <= range; y++)
+        {
+            for (int x = 0; x <= range; x++)
+            {
+                bool hit = false;
+                foreach (var sensor in sensors)
+                {
+                    var s = sensor.Item1;
+                    var distanceToSensor = manhattanDistance(s, (x, y));
+                    if (distanceToSensor > sensor.Item2) continue;
+                    x = (sensor.Item2 - Math.Abs(y - s.Item2)) + s.Item1;
+                    hit = true;
+                    break;
+                }
+                if (!hit)
+                {
+                    Console.WriteLine($"part2: {x*range + y}");
+                    return;
+                }
+            }
+        }
+
     }
 
     private static void part1()
